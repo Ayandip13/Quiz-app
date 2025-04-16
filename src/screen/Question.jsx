86679e28f4963a0d6e0190b7045831d2daeb87ec
Ayandip paul <152761756+Ayandip13@users.noreply.github.com>
2025-04-17 01:39:40 +0530

    add question screen

--- src/screen/Question.jsx
+++ src/screen/Question.jsx
@@ -1,12 +1,81 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, Pressable, Alert } from "react-native";
+import React, { useState } from "react";
+import { questions } from "../config/Question";
 
 const Question = () => {
-  return (
-    <View>
-      <Text>Question</Text>
-    </View>
-  )
-}
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const [selectedOption, setSelectedOption] = useState(null);
+  const [isCorrect, setIscorrect] = useState(null);
 
-export default Question
+  console.log({ isCorrect });
+
+  const handleNext = () => {
+    if (currentQuestionIndex === questions.length - 1) {
+      return;
+    }
+    setCurrentQuestionIndex(currentQuestionIndex + 1);
+  };
+
+  const handleOptionPress = (pressedOption) => {
+    setSelectedOption(pressedOption);
+    const isAnswerCorrect =
+      pressedOption === questions[currentQuestionIndex].correctAnswer;
+
+    setIscorrect(isAnswerCorrect);
+
+    if (isAnswerCorrect) {
+      setScore((prevScore) => prevScore + 10);
+    }
+  };
+
+  return (
+    <View style={{ marginTop: 28, padding: 10 }}>
+      <Text style={{ fontSize: 20, marginBottom: 18 }}>
+        {questions[currentQuestionIndex].question}
+      </Text>
+      {questions[currentQuestionIndex].options.map((item, index) => (
+        <Pressable
+          key={index}
+          onPress={() => handleOptionPress(item)}
+          style={[
+            {
+              borderColor: "#AA60C8",
+              borderWidth: 0.4,
+              padding: 14,
+              marginVertical: 5,
+              borderRadius: 4,
+            },
+            selectedOption === item && {
+              borderColor: isCorrect ? "#67AE6E" : "#FF6363",
+              backgroundColor: isCorrect? "#E3FBE4" : "#FFE4E4",
+            },
+          ]}
+        >
+          <Text
+            style={{
+              fontSize: 17,
+              color: "#000",
+            }}
+          >
+            {item}
+          </Text>
+        </Pressable>
+      ))}
+      <TouchableOpacity
+        style={{
+          backgroundColor: "#D69ADE",
+          paddingVertical: 10,
+          paddingHorizontal: 50,
+          marginTop: 30,
+          alignItems: "center",
+        }}
+        onPress={() => handleNext()}
+      >
+        <Text>Next question</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Question;
